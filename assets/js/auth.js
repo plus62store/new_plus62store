520293a36669bff400af553254f7b2bbed5c9052
plus62store <75934706+plus62store@users.noreply.github.com>
2025-03-14 11:40:25 +0700

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,21 +1,13 @@
-    const firebaseConfig = {
-    apiKey: "{{ site.API_KEY }}",
-    authDomain: "{{ site.AUTH_DOMAIN }}",
-    databaseURL: "{{ site.DATABASE_URL }}",
-    projectId: "{{ site.PROJECT_ID }}",
-    storageBucket: "{{ site.STORAGE_BUCKET }}",
-    messagingSenderId: "{{ site.SENDER_ID }}",
-    appId: "{{ site.APP_ID }}",
-    measurementId: "{{ site.MEASUREMENT_ID }}"
-    };firebase.initializeApp(firebaseConfig);
-firebase.auth().onAuthStateChanged(user => {
-  const protectedPages = [
-    "{{site.baseurl}}/LoggedIn/",
-    "{{site.baseurl}}/LoggedIn/akun",
-    "{{site.baseurl}}/LoggedIn/product/"
-  ];
-
-  if (!user && protectedPages.includes(window.location.pathname)) {
-    window.location.href = "{{site.baseurl}}/";
-  }
-});
+ const firebaseConfig = {
+ apiKey: "{{ site.API_KEY }}",
+ authDomain: "{{ site.AUTH_DOMAIN }}",
+ databaseURL: "{{ site.DATABASE_URL }}",
+ projectId: "{{ site.PROJECT_ID }}",
+ storageBucket: "{{ site.STORAGE_BUCKET }}",
+ messagingSenderId: "{{ site.SENDER_ID }}",
+ appId: "{{ site.APP_ID }}",
+ measurementId: "{{ site.MEASUREMENT_ID }}"
+};
+firebase.initializeApp(firebaseConfig);const auth=firebase.auth();const userSignedOutDiv=document.getElementById('user-signed-out');const userSignedInDiv=document.getElementById('user-signed-in');const userDisplayName=document.getElementById('user-display-name');const userEmail=document.getElementById('user-email');const signOutButton=document.getElementById('sign-out-button');function toggleUI(){if(auth.currentUser){userDisplayName.textContent=auth.currentUser.displayName;userEmail.textContent=auth.currentUser.email}else{location.href="{{site.baseurl}}/"}
+if(firebase.auth().currentUser.photoURL){document.querySelector('#userImg').src=firebase.auth().currentUser.photoURL}}
+signOutButton.addEventListener('click',()=>{auth.signOut()});auth.onAuthStateChanged(user=>{toggleUI()})
