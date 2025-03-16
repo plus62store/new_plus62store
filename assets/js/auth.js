726dbd4933e1928388fe0087ecd59c567ef41cd8
plus62store <75934706+plus62store@users.noreply.github.com>
2025-03-17 00:11:13 +0700

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -10,4 +10,10 @@
 };
 firebase.initializeApp(firebaseConfig);const auth=firebase.auth();const userSignedOutDiv=document.getElementById('user-signed-out');const userSignedInDiv=document.getElementById('user-signed-in');const userDisplayName=document.getElementById('user-display-name');const userEmail=document.getElementById('user-email');const signOutButton=document.getElementById('sign-out-button');function toggleUI(){if(auth.currentUser){userDisplayName.textContent=auth.currentUser.displayName;userEmail.textContent=auth.currentUser.email}else{location.href="{{site.baseurl}}/"}
 if(firebase.auth().currentUser.photoURL){document.querySelector('#userImg').src=firebase.auth().currentUser.photoURL}}
-signOutButton.addEventListener('click',()=>{auth.signOut()});auth.onAuthStateChanged(user=>{toggleUI()})
+signOutButton.addEventListener('click',()=>{auth.signOut()});auth.onAuthStateChanged(user=>{toggleUI()});
+
+if (window.PublicKeyCredential) {
+    console.log("WebAuthn didukung di browser ini!");
+} else {
+    console.log("WebAuthn tidak didukung di browser ini.");
+};
